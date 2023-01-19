2575a8bff9f5fdf16f860177f08b76270c04d429
Rogerio Pessanha <rogerioo_pessanha@yahoo.com.br>
2023-01-19 15:45:44 -0300

    att

--- repos/src/App.js
+++ repos/src/App.js
@@ -1,11 +1,14 @@
+import React from 'react';
 
-import React from 'react'  
-
-import Routes from './routes'
+import Routes from './routes';
+import GlobalStyle from './styles/global';
 
 function App() {
   return (
+    <>
+    <GlobalStyle/>
     <Routes/>
+    </>
   );
 }
 
--- repos/src/pages/Main/index.js
+++ repos/src/pages/Main/index.js
@@ -1,8 +1,26 @@
 
-import React from "react";
+import React from 'react';
+import { FaGithub, FaPlus } from 'react-icons/fa';
+import {Container, Form, SubmitButton} from './styles';
 
-export default function Main() {
+export default function Main(){
     return(
-        <h1>Main</h1>
+      <Container>
+        
+        <h1>
+          <FaGithub size={25}/>
+          Meus Repositorios
+        </h1>
+  
+        <Form onSubmit={()=>{}}>
+          <input type="text" placeholder="Adicionar Repositorios"/>
+  
+          <SubmitButton>
+            <FaPlus color="#FFF" size={14}/>
+          </SubmitButton>
+  
+        </Form>
+  
+      </Container>
     )
-}
+  }
