2e352a37df058537c64870e153a2c4753ed65723
Rogerio Pessanha <rogerioo_pessanha@yahoo.com.br>
2023-01-19 22:48:05 -0300

    att

--- repos/src/pages/Main/index.js
+++ repos/src/pages/Main/index.js
@@ -1,90 +1,92 @@
-
-import React, { useState, useCallback } from 'react';
+import React, {useState, useCallback} from 'react';
 import { FaGithub, FaPlus, FaSpinner, FaBars, FaTrash } from 'react-icons/fa';
-import { Container, Form, SubmitButton, List, DeleteButton } from './styles';
+import {Container, Form, SubmitButton, List, DeleteButton} from './styles';
 
 import api from '../../services/api';
 
-export default function Main() {
+export default function Main(){
 
-  const [newRepo, setNewRepo] = useState('')
-  const [repositorios, setRepositorios] = useState([])
-  const [loading, setLoading] = useState(false)
+  const [newRepo, setNewRepo] = useState('');
+  const [repositorios, setRepositorios] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-  const handleSubmit = useCallback((e) => {
+
+  const handleSubmit = useCallback((e)=>{
     e.preventDefault();
 
-    async function submit() {
+    async function submit(){
       setLoading(true);
-      try {
-        const response = await api.get(`repos/${newRepo}`)
-
+      try{
+        const response = await api.get(`repos/${newRepo}`);
+  
         const data = {
           name: response.data.full_name,
         }
-
-        setRepositorios([...repositorios, data])
-        setNewRepo('')
-      } catch (error) {
-        console.log(error)
-      } finally {
-        setLoading(false)
+    
+        setRepositorios([...repositorios, data]);
+        setNewRepo('');
+      }catch(error){
+        console.log(error);
+      }finally{
+        setLoading(false);
       }
 
     }
 
-    submit()
-  }, [newRepo, repositorios])
+    submit();
 
-  function handleInputChange(e) {
-    setNewRepo(e.target.value)
+  }, [newRepo, repositorios]);
+
+  function handleinputChange(e){
+    setNewRepo(e.target.value);
   }
 
-  const handleDelete = useCallback((repo) =>{
-    const find = repositorios.filter(r => r.name !== repo)
-    setRepositorios(find)
-  }, [repositorios])
+  const handleDelete = useCallback((repo)=> {
+    const find = repositorios.filter(r => r.name !== repo);
+    setRepositorios(find);
+  }, [repositorios]);
 
-  return (
+
+  return(
     <Container>
-
+      
       <h1>
-        <FaGithub size={25} />
+        <FaGithub size={25}/>
         Meus Repositorios
       </h1>
 
       <Form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Adicionar Repositorios"
-          value={newRepo}
-          onChange={handleInputChange}
+        <input 
+        type="text" 
+        placeholder="Adicionar Repositorios"
+        value={newRepo}
+        onChange={handleinputChange}
         />
 
         <SubmitButton loading={loading ? 1 : 0}>
           {loading ? (
-            <FaSpinner color='#fff' size={14} />
+            <FaSpinner color="#FFF" size={14}/>
           ) : (
-            <FaPlus color="#FFF" size={14} />
+            <FaPlus color="#FFF" size={14}/>
           )}
         </SubmitButton>
 
       </Form>
 
       <List>
-            {repositorios.map(repo => (
-              <li key={repo.name}>
-                <span>
-                  <DeleteButton onClick={() => handleDelete()}>
-                    <FaTrash size={14}/>
-                  </DeleteButton>
-                  {repo.name}
-                </span>
-                <a href="">
-                  <FaBars size={20}/>
-                </a>
-              </li>
-            ))}
+         {repositorios.map(repo => (
+           <li key={repo.name}>
+             <span>
+             <DeleteButton onClick={()=> handleDelete(repo.name) }>
+                <FaTrash size={14}/>
+             </DeleteButton>  
+             {repo.name}
+             </span>
+             <a href="">
+               <FaBars size={20}/>
+             </a>
+           </li>
+         ))} 
       </List>
 
     </Container>
--- repos/src/pages/Main/styles.js
+++ repos/src/pages/Main/styles.js
@@ -1,8 +1,7 @@
-
-import styled, {keyframes, css} from 'styled-components'
+import styled, {keyframes, css} from 'styled-components';
 
 export const Container = styled.div`
-    max-width: 700px;
+  max-width: 700px;
   background: #FFF;
   border-radius: 4px;
   box-shadow: 0 0 20px rgba(0,0,0, 0.2);
@@ -20,11 +19,12 @@ export const Container = styled.div`
     }
 
   }
+
+
 `;
 
 export const Form = styled.form`
-
-margin-top: 30px;
+  margin-top: 30px;
   display:flex;
   flex-direction: row;
 
@@ -38,22 +38,22 @@ margin-top: 30px;
 
 `;
 
+//Criando animcação do botao
 const animate = keyframes`
-from{
-  transform: rotate(0deg);
-}
+  from{
+    transform: rotate(0deg);
+  }
 
-to{
-  transform: rotate(360deg);
-}
-`
+  to{
+    transform: rotate(360deg);
+  }
+`;
 
 export const SubmitButton = styled.button.attrs(props => ({
   type: 'submit',
   disabled: props.loading,
 }))`
-
-background:#0D2636;
+  background:#0D2636;
   border: 0;
   border-radius: 4px;
   margin-left: 10px;
@@ -62,49 +62,57 @@ background:#0D2636;
   justify-content: center;
   align-items: center;
 
+
   &[disabled]{
     cursor: not-allowed;
     opacity: 0.5;
   }
 
 
-${props => props.loading &&
-  css`
-  svg{
-    animation: ${animate} 2s linear infinite;
+  ${props => props.loading &&
+    css`
+      svg{
+        animation: ${animate} 2s linear infinite;
+      }
+    `
   }
-  `
-}
+
 
 `;
 
 export const List = styled.ul`
-  list-style: none;
+  list-style:none;
   margin-top: 20px;
 
   li{
     padding: 15px 0;
     display: flex;
-    flex-direction: row;
+    flex-direction:row;
     align-items: center;
     justify-content: space-between;
 
     & + li{
-      border-top: 1px solid #000;
+      border-top: 1px solid #eee;
     }
 
     a{
-      color: #0D2636;
+      color:#0D2636;
       text-decoration: none;
     }
+
+
   }
-`
+
+`;
+
 
 export const DeleteButton = styled.button.attrs({
-  type: 'button'
+  type:'button'
 })`
-background: transparent;
-color: #0d2636;
-border:0;
-padding:8px 7px;
+  background: transparent;
+  color:#0D2636;
+  border:0;
+  padding: 8px 7px;
+  outline:0;
+  border-radius: 4px;
 `;
