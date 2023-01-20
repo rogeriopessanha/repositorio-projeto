da554ee024fc98c786bd19c8dd62ab94a41c4f6e
Rogerio Pessanha <rogerioo_pessanha@yahoo.com.br>
2023-01-20 01:34:26 -0300

    att

--- repos/src/pages/Repositorio/index.js
+++ repos/src/pages/Repositorio/index.js
@@ -1,23 +1,23 @@
-import React, { useState, useEffect } from 'react';
-import { Container, Owener, Loading, BackButton } from './styles';
-import {FaArrowLeft} from 'react-icons/fa'
+import React, {useState, useEffect} from 'react';
+import {Container, Owner, Loading, BackButton, IssuesList} from './styles';
+import { FaArrowLeft } from 'react-icons/fa';
 import api from '../../services/api';
 
-export default function Repositorio({ match }) {
+export default function Repositorio({match}){
 
   const [repositorio, setRepositorio] = useState({});
   const [issues, setIssues] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-
-    async function load() {
+  useEffect(()=> {
+    
+    async function load(){
       const nomeRepo = decodeURIComponent(match.params.repositorio);
 
       const [repositorioData, issuesData] = await Promise.all([
         api.get(`/repos/${nomeRepo}`),
         api.get(`/repos/${nomeRepo}/issues`, {
-          params: {
+          params:{
             state: 'open',
             per_page: 5
           }
@@ -26,6 +26,8 @@ export default function Repositorio({ match }) {
 
       setRepositorio(repositorioData.data);
       setIssues(issuesData.data);
+      console.log(issuesData.data);
+
       setLoading(false);
 
     }
@@ -34,30 +36,53 @@ export default function Repositorio({ match }) {
 
   }, [match.params.repositorio]);
 
-  if (loading) {
-
-    return (
+  
+  if(loading){
+    return(
       <Loading>
         <h1>Carregando...</h1>
       </Loading>
     )
-
   }
-
-  return (
+  
+  return(
     <Container>
-      <BackButton to='/'>
-        <FaArrowLeft color='#000' size={35}/>
-      </BackButton>
-      <Owener>
-        <img
-          src={repositorio.owner.avatar_url}
-          alt={repositorio.owner.login}
-        />
+        <BackButton to="/">
+          <FaArrowLeft color="#000" size={30} />
+        </BackButton>
 
-        <h1>{repositorio.name}</h1>
-        <p>{repositorio.description}</p>
-      </Owener>
+        <Owner>
+          <img 
+          src={repositorio.owner.avatar_url} 
+          alt={repositorio.owner.login} 
+          />
+          <h1>{repositorio.name}</h1>
+          <p>{repositorio.description}</p>
+        </Owner>
+
+        <IssuesList>
+          {issues.map(issue => (
+            <li key={String(issue.id)}>
+              <img src={issue.user.avatar_url} alt={issue.user.login} />
+
+              <div>
+                <strong>
+                  <a href={issue.html_url}>{issue.title}</a>
+
+                  {issue.labels.map(label => (
+                    <span key={String(label.id)}>{label.name}</span>
+                  ))}
+
+                </strong>
+
+                <p>{issue.user.login}</p>
+
+              </div>
+
+            </li>
+          ))}
+        </IssuesList>
+
     </Container>
   )
 }
